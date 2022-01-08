7e76c679909fa70e4c2f345d2162c3338d14700c
Butcher Tibi <47886094+ButcherTibi@users.noreply.github.com>
2022-01-08 17:51:14 +0200

    Wish Github Desktop would have a init repo option
    Also made nice README.md.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# ButchersToolbox
- 
+## ButchersToolbox
+A collection of stuff that I commonly use in my projects.
+
+### Table of Contents
+1. `utf8_string` library
+2. `Console` namespace for text formating
+3. `Filesys` for reading files and handling filesystem paths
+
+### Requirements
+Visual Studio 2022 with C++ 20 standard.
+
+### utf8_string library
+One day I got really annoyed about the fact that C++ does not have a nice and easy to use unicode string library like C#.
+It was easier than I thought.
+**This thing is getting good**
+
+### Console
+Ever wandered how to print to console colored text ?
+Did you know you need to configure the console terminal, to enable UTF-8 to work ?
+
+### Filesys
+It simply started with my hate of using regular expressions to handle filesystem paths.
+I used this extensively to read compiled HLSL with no extra copies directly into a `std::vector`, also used the Win32 `FILE_FLAG_SEQUENTIAL_SCAN` to make read faster.
+*Needs more work*
---
+++ TestingClient/main.cpp
@@ -0,0 +1,168 @@
+﻿
+// Standard
+#include <stdio.h>
+#include <iostream>
+
+// Mine
+#include <ButchersToolbox\Console.hpp>
+#include <ButchersToolbox\utf_string.hpp>
+
+
+namespace utf8string_tests {
+
+	void testInsertAfter()
+	{
+		// Before
+		{
+			utf8string str = u8"ăîșț";
+			utf8string_iter str_iter = str.before();
+			utf8string new_content = u8"Ă";
+
+			utf8string new_str = str;
+			new_str.insertAfter(str_iter, new_content);
+
+			if (new_str != u8"Ăăîșț") {
+				throw std::exception();
+			}
+		}
+
+		// Begin
+		{
+			utf8string str = u8"ăîșț";
+			utf8string_iter str_iter = str.begin();
+			utf8string new_content = u8"Ă";
+
+			utf8string new_str = str;
+			new_str.insertAfter(str_iter, new_content);
+
+			if (new_str != u8"ăĂîșț") {
+				throw std::exception();
+			}
+		}
+
+		// Last
+		{
+			utf8string str = u8"ăîșț";
+			utf8string_iter str_iter = str.last();
+			utf8string new_content = u8"Ă";
+
+			utf8string new_str = str;
+			new_str.insertAfter(str_iter, new_content);
+
+			if (new_str != u8"ăîșțĂ") {
+				throw std::exception();
+			}
+		}
+	}
+
+	void testPushAndPrepend()
+	{
+		// Append
+		{
+			utf8string str = u8"Cârnați";
+			str.push(u8" cu mămăliguță");
+
+			if (str != u8"Cârnați cu mămăliguță") {
+				throw std::exception();
+			}
+		}
+		
+		// Prepend
+		{
+			utf8string str = u8" cu mămăliguță";
+			str.prepend(u8"Cârnați");
+
+			if (str != u8"Cârnați cu mămăliguță") {
+				throw std::exception();
+			}
+		}
+	}
+
+	void testForLoop()
+	{
+		utf8string str = u8"ăĂîșț";
+
+		uint32_t index = 0;
+		for (auto i = str.begin(); i < str.end(); i++) {
+
+			auto character = i.get();
+
+			switch (index) {
+			case 0: {
+				if (character != u8"ă") {
+					throw std::exception();
+				}
+				break;
+			}
+			case 1: {
+				if (character != u8"Ă") {
+					throw std::exception();
+				}
+				break;
+			}
+			case 2: {
+				if (character != u8"î") {
+					throw std::exception();
+				}
+				break;
+			}
+			case 3: {
+				if (character != u8"ș") {
+					throw std::exception();
+				}
+				break;
+			}
+			case 4: {
+				if (character != u8"ț") {
+					throw std::exception();
+				}
+				break;
+			}
+			}
+
+			index++;
+		}
+	}
+
+	void testErase()
+	{
+		// Pop
+		{
+			utf8string str = u8"ăĂîșț";
+			str.pop(3);
+
+			if (str != u8"ăĂ") {
+				throw std::exception();
+			}
+		}
+
+		// Pop Front
+		{
+			utf8string str = u8"ăĂîșț";
+			str.popFront(3);
+
+			if (str != u8"șț") {
+				throw std::exception();
+			}
+		}
+	}
+
+	void testEverything()
+	{
+		testInsertAfter();
+		testPushAndPrepend();
+		testErase();
+		testForLoop();
+	}
+}
+
+
+int main()
+{
+	Console::configureForUTF8();
+
+	utf8string_tests::testEverything();
+
+	utf8string str = u8"Ok";
+	printf("%s \n", str.c_str());
+}
